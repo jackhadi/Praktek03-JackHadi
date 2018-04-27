721a0ce1aaa1b9dc5b0867bf5062d0d695c0bfe1
jackhadi <jackhadi@gmail.com>
2018-04-27 00:14:57 -0700

    menggunakan method get

--- src/praktek03/jackhadi/aksi.java
+++ src/praktek03/jackhadi/aksi.java
@@ -9,6 +9,13 @@ public class aksi {
         e.setHarga(9000);
         
         e.cetakInfo();
+        
+         System.out.println("Jenisnya \t: ");
+         System.out.println(e.getJenis());
+         System.out.println("Merknya \t: ");
+         System.out.println(e.getMerk());
+         System.out.println("Harganya \t: ");
+         System.out.println(e.getHarga());
     }
     
 }

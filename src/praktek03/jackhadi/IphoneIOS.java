5caa6158391ee10efac614681062fd04784a9a43
jackhadi <jackhadi@gmail.com>
2018-04-27 00:19:12 -0700

    attribute dan method

--- src/praktek03/jackhadi/IphoneIOS.java
+++ src/praktek03/jackhadi/IphoneIOS.java
@@ -3,4 +3,11 @@ package praktek03.jackhadi;
 
 public class IphoneIOS extends Handphone {
     
+    private String jenisIphone;
+    
+    void iniIphone (){
+        System.out.println("Ulun salah tukar, maka tatukar samsung s9, manyasal heh");
+        
+            }
+    
 }

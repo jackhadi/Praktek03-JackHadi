3efc5841148f9a0264d927106364bc2dda43dd57
jackhadi <jackhadi@gmail.com>
2018-04-27 00:06:38 -0700

    instance object dan memanggil method

--- src/praktek03/jackhadi/aksi.java
+++ src/praktek03/jackhadi/aksi.java
@@ -1,5 +1,14 @@
 package praktek03.jackhadi;
 
 public class aksi {
+    public static void main(String[] args) {
+        Handphone e = new Handphone();
+         
+        e.jenis="Andorid";
+        e.merk="Meizu M5S";
+        e.harga=10000;
+        
+        e.cetakInfo();
+    }
     
 }

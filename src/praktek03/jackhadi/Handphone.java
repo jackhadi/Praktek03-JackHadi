a9b66b102f19c1ae8f177407601c338efe3b6c46
jackhadi <jackhadi@gmail.com>
2018-04-26 23:59:58 -0700

    membuat method cetakInfo

--- src/praktek03/jackhadi/Handphone.java
+++ src/praktek03/jackhadi/Handphone.java
@@ -4,4 +4,10 @@ public class Handphone {
      String jenis;
      String merk;
      double harga;
+     
+     void cetakInfo(){
+         System.out.println("Jenis \t: "+jenis+"\n"+
+            "Merk \t: "+merk+"\n"+
+            "Harga \t: "+harga);
+     }
 }
